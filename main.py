172554c9f8ac4407bad6203911ea3174ea942321
Hector DelaRosa <hdelaros@gmail.com>
2023-04-13 23:37:11 -0400

    Added price alert logic, formatted email message and refactored code.

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from bs4 import BeautifulSoup
 
 MY_EMAIL = os.environ['EMAIL']
 PASS = os.environ['PASS']
-
+MY_PRICE = 100
+URL ='https://www.amazon.com/Ninja-Kitchen-BL770-Processor-Smoothies/dp/B00939I7EK/ref=sr_1_5?' \
+     'keywords=ninja+blenders&qid=1681270349&s=home-garden&sprefix=ninja+%2Cgarden%2C194&sr=1-5'
 header = {
-
     "sec-ch-ua": '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "macOS",
@@ -16,21 +17,20 @@ header = {
     "Accept-Language": "en-US,en;q=0.9"
 }
 
-response = requests.get('https://www.amazon.com/Ninja-Kitchen-BL770-Processor-Smoothies/dp/B00939I7EK/ref=sr_1_5?'
-                        'keywords=ninja+blenders&qid=1681270349&s=home-garden&sprefix=ninja+%2Cgarden%2C194&sr=1-5',
-                        headers=header)
+response = requests.get(URL, headers=header)
 
 web_page_data = response.text
-
 soup = BeautifulSoup(web_page_data, 'html.parser')
 
-price = float(soup.select(selector='.a-offscreen')[0].getText()[1::])
+curr_float_price = float(soup.select(selector='.a-offscreen')[0].getText()[1::])
+product_title = soup.find(id='productTitle').getText()
 
-with smtplib.SMTP('smtp.gmail.com') as connection:
-    connection.starttls()
-    connection.login(user=MY_EMAIL, password=PASS)
-    connection.sendmail(from_addr=MY_EMAIL,
-                        to_addrs=MY_EMAIL,
-                        msg='Subject:Sample\n\nThis is the body of the email'
-                        )
-
+if curr_float_price <= MY_PRICE:
+    message = f'{product_title} is now ${curr_float_price}\n{URL}'
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASS)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs=MY_EMAIL,
+                            msg=f'Subject:Amazon Price alert for {product_title}\n\n{message}'
+                            )
